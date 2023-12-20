d8e7b1d10fd3a1cbb166a3c4e089151528ea49fd
fudwaha <152516493+fudwaha@users.noreply.github.com>
2023-12-20 21:53:35 +0700

    Add files via upload

---
+++ user_validator.php
@@ -0,0 +1,90 @@
+<?php 
+
+class UserValidator {
+
+  public $data;
+  public $errors = [];
+  public static $fields = ['username', 'email'];
+
+  public function __construct($post_data){
+    $this->data = $post_data;
+  }
+
+  public function validateForm(){
+
+    foreach(self::$fields as $field){
+      if(!array_key_exists($field, $this->data)){
+        trigger_error("'$field' is not present in the data");
+        return;
+      }
+    }
+
+    $this->validateUsername();
+    $this->validateEmail();
+    return $this->errors;
+
+  }
+
+  public function validateUsername(){
+
+    $val = trim($this->data['username']);
+
+    if(empty($val)){
+      $this->addError('username', 'username cannot be empty');
+    } else {
+      if(!preg_match('/^[a-zA-Z0-9]{6,12}$/', $val)){
+        $this->addError('username','username must be 6-12 chars & alphanumeric');
+      } 
+    }
+  }
+
+  public function validateEmail(){
+
+    $val = trim($this->data['email']);
+
+    if(empty($val)){
+      $this->addError('email', 'email cannot be empty');
+    } else {
+      if(!filter_var($val, FILTER_VALIDATE_EMAIL)){
+        $this->addError('email', 'email must be a valid email address');
+      } 
+    }
+  }
+
+  public function addError($key, $val){
+    $this->errors[$key] = $val;
+  }
+
+}
+
+class handleLoginSuccess extends UserValidator {
+  public function validateUsername(){
+
+    $val = trim($this->data['username']);
+
+    if(empty($val)){
+      $this->addError('username', 'username cannot be empty');
+    } else {
+      if(!preg_match('/^[a-zA-Z0-9]{6,12}$/', $val)){
+        $this->addError('username','username must be 6-12 chars & alphanumeric');
+      } else {
+        parent::addError('username', 'Login Success');
+      }
+    }
+  }
+
+  public function validateEmail(){
+
+    $val = trim($this->data['email']);
+
+    if(empty($val)){
+      $this->addError('email', 'email cannot be empty');
+    } else {
+      if(!filter_var($val, FILTER_VALIDATE_EMAIL)){
+        $this->addError('email', 'email must be a valid email address');
+      } else {
+        parent::addError('email', 'Login Success');
+      }
+    }
+  }
+}
